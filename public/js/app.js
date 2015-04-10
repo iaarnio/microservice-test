b295c2ba55f1c8d9e6a6949ecd0a230bd83a0ac3
Ilari Aarnio <ilari.aarnio@codesys.fi>
2015-04-10 23:31:02 +0300

    change ui

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,11 @@
 
   var app = angular.module('job.view', ['ngRoute']);
 
-  app.config(['$routeProvider', function($routeProvider) {
+  app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-    .when('/ui', {
-      template: '<p>direct html</p>'
-  //    templateUrl: '/partials/view.html',
-      controller: 'UiController'
-    })
-    .when('/', {
-      controller: 'UiController',
-      template: '<h2>We are home</h2>'
-    });
+      .when('/', {
+        controller: 'UiController',
+        templateUrl: '/partials/view.html'
+      });
   }])
 })();
